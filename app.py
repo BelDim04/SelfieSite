d6abdda40e6b2d5be25f7b09d68aa4ec50e0e77a
BelDim04 <beldim04@gmail.com>
2022-05-17 12:15:33 +0300

    initial commit

---
+++ app.py
@@ -0,0 +1,43 @@
+from datetime import datetime
+from flask import Flask, render_template, request, redirect
+from flask_sqlalchemy import SQLAlchemy
+import cv2
+
+app = Flask(__name__)
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///media.db'
+db = SQLAlchemy(app)
+
+
+class Media(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    path = db.Column(db.String(200), nullable=False)
+
+    def __init__(self, path):
+        self.path = path
+
+
+@app.route('/', methods=['POST', 'GET'])
+def hello_world():  # put application's code here
+    if request.method == 'POST':
+        try:
+            cap = cv2.VideoCapture(0)
+            for i in range(10):
+                cap.read()
+            ret, frame = cap.read()
+            path = 'pictures/'+str(datetime.utcnow()) + '.png'
+            path = path.replace(' ', '_')
+            cv2.imwrite('static/'+path, frame)
+            cap.release()
+            new_picture = Media(path)
+            db.session.add(new_picture)
+            db.session.commit()
+            return redirect('/')
+        except:
+            return 'Error'
+    else:
+        pictures = Media.query.all()
+        return render_template('index.html', pictures=pictures)
+
+
+if __name__ == '__main__':
+    app.run()
